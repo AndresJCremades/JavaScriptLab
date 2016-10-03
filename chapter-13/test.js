b3f009b9a3f72ac5fbd228386379ac568426bf5b
AndresJCremades <andres.j.cremades@gmail.com>
2016-10-03 14:50:07 +0200

    First commit

---
+++ chapter-13/test.js
@@ -0,0 +1,14 @@
+function elt(type) {
+  var node = document.createElement(type);
+  for (var i = 1; i < arguments.length; i++) {
+    var child = arguments[i];
+    if (typeof child == "string")
+      child = document.createTextNode(child);
+    node.appendChild(child);
+  }
+  return node;
+}
+
+document.getElementById("quote").appendChild(
+    elt("footer", "—", elt("strong", "Karl Popper"),", preface to the second editon of ", elt("em", "The Open Society and Its Enemies"),", 1950")
+    );
